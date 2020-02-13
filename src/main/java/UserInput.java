2b514927b7dff6181f785f93922449a3e92ecd74
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-13 13:10:47 -0600

    -

--- src/main/java/UserInput.java
+++ src/main/java/UserInput.java
@@ -6,17 +6,15 @@ public class UserInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Something: ");
-        String input = sc.nextLine();
-        System.out.println("You Entered: " + input);
+        String userInput = sc.nextLine();
+        System.out.println("You Entered: " + userInput);
 
-        if (!StringUtils.isNumeric(input)) { // Checks if the CharSequence contains only Unicode digits. A decimal point is not a Unicode digit and returns false.
-            System.out.println(input + " is not a number");
-
+        if (!StringUtils.isNumeric(userInput)) { // Checks if the CharSequence contains only Unicode digits. A decimal point is not a Unicode digit and returns false.
+            System.out.println(userInput + " isn't a number");
         } else {
-            System.out.println(input + " is a number");
+            System.out.println(userInput + " is a number");
         }
-
-        System.out.println("Flipped Case: " + StringUtils.swapCase(input)); // Swaps the case of a String changing upper and title case to lower case, and lower case to upper case.
-        System.out.println("Reversed: " + StringUtils.reverse(input)); // Reverses a String as per StringBuilder.reverse().
+        System.out.println("Flipped Case: " + StringUtils.swapCase(userInput)); // Swaps the case of a String changing upper and title case to lower case, and lower case to upper case.
+        System.out.println("Reversed: " + StringUtils.reverse(userInput)); // Reverses a String as per StringBuilder.reverse().
     }
 }
